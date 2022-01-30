638b3b1d695324dda6dac0e6a533636778100342
Caio <caio-h10@hotmail.com>
2022-01-30 11:18:42 -0300

    .

--- Curso Java/aula06/Argumentos.java
+++ Curso Java/aula06/Argumentos.java
@@ -1,5 +1,5 @@
 class Argumentos {
     public static void main (String[] args){
-        System.out.println("Você digitou " + args[0])
+        System.out.println("Você digitou " + args[0]);
     }
 }
